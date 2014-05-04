b0d3ff136bbee97f0d75892ed5a6cf0113f875b2
Lloyd <l.harischandra@uws.edu.au>
2014-05-05 00:07:35 +1000

    Junit test case for DefaultDispenser

---
+++ src/test/java/atm/service/DispenserTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package atm.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lloyd
+ *
+ */
+public class DispenserTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link atm.service.DefaultDispenser#withdraw(int)}.
+	 */
+	@Test
+	public void testWithdraw() {
+		fail("Not yet implemented");
+	}
+
+}
